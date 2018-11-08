1513218428c009acb40637f8df58be304270ff3b
szang <szang@ciudad-azteca.com>
2018-11-08 22:47:18 +0100

    copy source code from version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { EntryComponent } from './entry/entry.component';
+import { UserComponent } from './user/user.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    EntryComponent,
+    UserComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
